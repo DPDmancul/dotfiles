dc6455b33b307dcb28da36d11daba24a97ff2671
user.email <davide.peressoni@tuta.io>
2022-12-28 09:15:13 +0100

    Added gpg key for work

--- PereWork/home/git.nix
+++ PereWork/home/git.nix
@@ -9,6 +9,7 @@
         user = {
           name = "Davide Peressoni";
           email = "d.peressoni@mvlabs.it";
+          signingKey = "C326CCA55860233AA83778F6047114319C2F3D66";
         };
       };
     }
