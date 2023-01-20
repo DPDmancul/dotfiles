ae9d9073609579ee492132c75b678b1d498965a4
user.email <davide.peressoni@tuta.io>
2023-01-20 14:25:48 +0100

    removed bugged extension

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -22,7 +22,7 @@
           "{446900e4-71c2-419f-a6a7-df9c091e268b}" = ext "bitwarden-password-manager";
           # "vim-vixen@i-beam.org" = ext "vim-vixen";
           "{7be2ba16-0f1e-4d93-9ebc-5164397477a9}" = ext "videospeed";
-          "arklove@qq.com" = ext "git-master";
+          # "arklove@qq.com" = ext "git-master";
           "proxydocile@unipd.it"= {
             installation_mode = "force_installed";
             install_url = "https://softwarecab.cab.unipd.it/proxydocile/proxydocile.xpi";
