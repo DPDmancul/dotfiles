c56147e651614a6737576507b13cbc25f9c45028
user.email <davide.peressoni@tuta.io>
2023-03-18 11:49:11 +0100

    small temp fix

--- PereWork/system/autorandr.nix
+++ PereWork/system/autorandr.nix
@@ -2,13 +2,18 @@
 {
   services.autorandr = {
     profiles = {
-      "scrivanie" = {
-        config = {
-          DP-6.position = "0x0";
-          DP-4.position = "2560x0";
-          eDP-1.position = "5120x0";
-        };
-      };
+      # "scrivanie" = {
+      #   fingerprint = {
+      #     DP-6 = "";
+      #     DP-4 = "";
+      #     eDP-1 = "";
+      #   };
+      #   config = {
+      #     DP-6.position = "0x0";
+      #     DP-4.position = "2560x0";
+      #     eDP-1.position = "5120x0";
+      #   };
+      # };
     };
   };
 
