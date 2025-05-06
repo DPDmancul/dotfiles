5e5d099c3be7d0afb3f2c2e14b7131d3bd0aa8ba
DPDmancul <davide.peressoni@tuta.io>
2025-05-06 23:01:46 +0200

    update musescore

--- flake/pkgs/musescore-appimage.nix
+++ flake/pkgs/musescore-appimage.nix
@@ -1,10 +1,10 @@
 { appimageTools, fetchurl }:
 let
   pname = "musescore-appimage";
-  version = "4.4.4";
+  version = "4.5.2";
   src = fetchurl {
-    url = "https://cdn.jsdelivr.net/musescore/v${version}/MuseScore-Studio-${version}.243461245-x86_64.AppImage";
-    hash = "sha256-g5mb9mPqh5lDV2wIBugzFMKtjJzGuXm5mIZVvsyRBh4=";
+    url = "https://cdn.jsdelivr.net/musescore/v${version}/MuseScore-Studio-${version}.251141401-x86_64.AppImage";
+    hash = "sha256-0BC2Rkx4tNojB3608Jb5Sa5ousTICaiwCKDPv0jiYKU=";
   };
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
 in
