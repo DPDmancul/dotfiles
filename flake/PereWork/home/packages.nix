819b1c7022571e977c94d4a65f0338fe22e57bf8
DPDmancul <davide.peressoni@tuta.io>
2026-06-16 15:06:54 +0200

    code-review-graph

--- flake/PereWork/home/packages.nix
+++ flake/PereWork/home/packages.nix
@@ -27,6 +27,33 @@
     bruno
     wireshark
     unfree.claude-code
+    (python3Packages.buildPythonPackage rec {
+      pname = "code-review-graph";
+      version = "2.3.6";
+      src = fetchFromGitHub {
+        owner = "tirth8205";
+        repo = pname;
+        tag = "v${version}";
+        hash = "sha256-akuk4UHOTfw66dnuAeqoCkqF/JzsHqSzoTk5MQhEd0o=";
+      };
+      pyproject = true;
+      build-system = with python3Packages; [
+        hatchling
+      ];
+      dependencies = with python3Packages; [
+        mcp
+        fastmcp
+        networkx
+        tree-sitter
+        tree-sitter-language-pack
+        watchdog
+      ];
+      pythonRelaxDeps = [
+        "fastmcp"
+        "tree-sitter-language-pack"
+        "watchdog"
+      ];
+     })
   ];
 
   #<<<PereWork/home/packages>>>
